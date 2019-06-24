50596ca13dc138449d74c95bd712f1dddc0bdeb7
willieLjohnson <liwa.johnson@gmail.com>
2019-06-24 18:50:52 -0400

    Update running animation based on player input

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+  public Animator animator;
+
   // Start is called before the first frame update
   void Start()
   {
@@ -13,6 +16,7 @@ public class Movement : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
     Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
     transform.position = transform.position + horizontal * Time.deltaTime;
   }
